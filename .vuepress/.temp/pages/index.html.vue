5021c0fa65ce37d88ab2bdbeaaaf2f8d08a38617
Yu MORIYA <euro21st@gmail.com>
2022-03-14 19:01:29 +0900

    Fix directory rules

--- .vuepress/.temp/pages/index.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="square-enix-music-channel-リンク集" tabindex="-1"><a class="header-anchor" href="#square-enix-music-channel-リンク集" aria-hidden="true">#</a> SQUARE ENIX MUSIC Channel リンク集</h1>
-</template>
